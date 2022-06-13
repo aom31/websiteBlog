8c5dabbf929fd232dfe2408d9fd64af69ca79bff
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-13 19:28:31 +0700

    category

--- myprojectweb/blogs_app/models.py
+++ myprojectweb/blogs_app/models.py
@@ -1,3 +1,17 @@
+from distutils.command.upload import upload
+from unicodedata import category
 from django.db import models
-
+from category.models import CategoryBlog
 # Create your models here.
+class Blogs(models.Model) :
+     name = models.CharField(max_length=255)
+     description = models.TextField()
+     content = models.TextField()
+     category = models.ForeignKey(CategoryBlog, on_delete= models.CASCADE)
+     writer = models.CharField(max_length=255)
+     views = models.IntegerField(default=0)
+     image = models.ImageField(upload_to = "blogsImgFold", blank= True)
+     create = models.DateTimeField(auto_now_add=True)
+     
+     def __str__(self):
+          return self.name
--- myprojectweb/blogs_app/views.py
+++ myprojectweb/blogs_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from category.models import CategoryBlog
 # Create your views here.
 
 def index(request):
-    return render(request, "frontEnd/index.html")
+    categories = CategoryBlog.objects.all()
+    return render(request, "frontEnd/index.html", {'categories' : categories})
